06af95cbc1a336b710dd0bb82fdb5fb1f61299e4
Daniel Leskosky <dleskosky@gmail.com>
2021-07-25 13:42:15 -0400

    alphabetical strings

---
+++ AlphabeticalStrings.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class AlphabeticalStrings {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        in.nextLine();
+        while (t-- > 0) {
+            String s = in.nextLine();
+            int n = s.length(), j;
+            for (j = 0; j < n; j++) {
+                if (s.charAt(j) == 'a') {
+                    break;
+                }
+            }
+            if (j == n) {
+                System.out.println("NO");
+            } else {
+                int i = 1;
+                int l = j - 1, r = j + 1;
+                while (i < n) {
+                    if (l >= 0 && s.charAt(l) == (char) (i + 'a')) {
+                        l--;
+                    } else if (r < n && s.charAt(r) == (char) (i + 'a')) {
+                        r++;
+                    }
+                    i++;
+                }
+                if (l == -1 && r == n) {
+                    System.out.println("YES");
+                } else {
+                    System.out.println("NO");
+                }
+            }
+        }
+        in.close();
+    }
+}
