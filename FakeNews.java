c001c0701251769e73baa4ce77b5e355eb158331
Daniel Leskosky <dleskosky@gmail.com>
2021-07-24 22:13:27 -0400

    fake news

---
+++ FakeNews.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+// n = s.length()
+// Time: O(n)
+// Space: O(1)
+public class FakeNews {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        String h = "heidi";
+        int j = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == h.charAt(j)) {
+                j++;
+                if (j == h.length()) {
+                    break;
+                }
+            }
+        }
+        if (j == h.length()) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+        in.close();
+    }
+}
